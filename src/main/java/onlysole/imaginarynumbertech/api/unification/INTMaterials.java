be70632e447dca2b34af951f08ed70a18fc9a8b3
Only_EX <3107800043@qq.com>
2024-12-29 22:07:13 +0800

    构建 6.1/?

--- src/main/java/onlysole/imaginarynumbertech/api/unification/INTMaterials.java
+++ src/main/java/onlysole/imaginarynumbertech/api/unification/INTMaterials.java
@@ -53,76 +53,72 @@ public class INTMaterials {
     }
 
     //材料
+
+    //#---------虚数---------#
+
+
+    //#---------合金---------#
+    public static Material QuantumAlloy;
+    public static Material MARM200Steel;
+    public static Material ElectrumFlux;
+    public static Material Shirabon;
+    public static Material Lumium;
+    public static Material Enderium;
+    public static Material Signalum;
+    public static Material Mithril;
+    public static Material AstralTitanium;
+    public static Material CelestialTungsten;
+    public static Material Rhugnor;
+    public static Material Hypogen;
+    public static Material DegenerateRhenium;
+    public static Material Taranium;
+    public static Material Legendarium;
+
+
+
+    //#---------粉-----------#
+    public static Material DragonDust;
+    public static Material CopperSulfatePentahydrate;
+    public static Material CopperSulfate;
+    public static Material CyanoaceticAcid;
+    public static Material SodiumCyanide;
+
+
+
+    //#---------虚空---------#
+    public static Material Quantium;
     public static Material BlackdWarfMatter;
     public static Material WhitedWarfMatter;
     public static Material Infinity;
     public static Material CosmicNeutronium;
     public static Material Spacetime;
-    public static Material Universium;
     public static Material Eternity;
     public static Material MagnetohydrodynamicallyConstrainedStarMatter;
-    public static Material DraconiumAwakened;
-    public static Material Draconium;
-    public static Material Lumium;
-    public static Material Enderium;
-    public static Material Signalum;
-    public static Material Mithril;
-    public static Material Shirabon;
-    public static Material AstralTitanium;
-    public static Material CelestialTungsten;
-    public static Material Rhugnor;
-    public static Material ElectrumFlux;
-    public static Material Hypogen;
-    public static Material MARM200Steel;
+    public static Material Universium;
     public static Material TranscendentMetal;
-    public static Material DegenerateRhenium;
-    public static Material Vibranium;
-    public static Material Adamantium;
-    public static Material DraconiumChaotic;
-    public static Material Orichalcum;
     public static Material CrystalMatrix;
-    public static Material Taranium;
+
+    //#---------魔法---------#
+    public static Material Draconium;
+    public static Material DraconiumAwakened;
+    public static Material DraconiumChaotic;
     public static Material Ichorium;
     public static Material IchorLiquid;
+    public static Material Orichalcum;
+    public static Material Vibranium;
+    public static Material Adamantium;
     public static Material VoidMetal;
-    public static Material Legendarium;
-    public static Material DragonDust;
-    public static Material DragonTear;
-    public static Material Quantium;
-    public static Material QuantumAlloy;
-    //public static Material ;
-    //public static Material ;
-    //public static Material ;
-    //public static Material ;
-    //public static Material ;
-    //public static Material ;
-    //public static Material ;
-    //public static Material ;
-    //public static Material ;
 
-    //流体
-    //public static Material ;
-    //public static Material ;
-    //public static Material ;
-    //public static Material ;
-    //public static Material ;
-    //public static Material ;
-    //public static Material ;
-    //public static Material ;
-    public static Material PineOil;
-    public static Material BlazingPyrotheum;
-    public static Material GelidCryotheum;
-    public static Material DraconiumSlagSolution;
-    public static Material ResidualDraconiumSolution;
+
+
+
+
+    //#---------流体---------#
     public static Material MutatedIivingSolder;
     public static Material EthylCyanoacrylateSuperGlue;
     public static Material CyanoacrylatePolymer;
     public static Material EthylCyanoacetate;
-    public static Material CopperSulfatePentahydrate;
-    public static Material CopperSulfate;
     public static Material SolidAcidCatalystMixture;
-    public static Material CyanoaceticAcid;
-    public static Material SodiumCyanide;
     public static Material ChloroaceticMixture;
     public static Material TrichloroaceticAcid;
     public static Material DichloroaceticAcid;
@@ -146,7 +142,14 @@ public class INTMaterials {
     public static Material DragonBreath;
     public static Material ConcentrateDragonBreath;
     public static Material DragonBlood;
+    public static Material DragonTear;
     public static Material NitratedDragonDustSolution;
+    public static Material ResidualDraconiumSolution;
+    public static Material DraconiumSlagSolution;
+    public static Material BlazingPyrotheum;
+    public static Material GelidCryotheum;
+    public static Material PineOil;
+
 
 
 
